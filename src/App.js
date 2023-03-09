566ac486fb3845c5b18c29e1474920e08f53fd5e
Archie Fayzullaev <fayzullaevh@gmail.com>
2023-03-08 23:50:06 -0600

    fix: removed s and removed slash

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Archie first time.
+          Edit <code>src/App.js</code> and save to reload Archie second time.
         </p>
         <a
           className="App-link"
