14218a482a42f1ecf2924173055007afcfa1d55f
Archie Fayzullaev <fayzullaevh@gmail.com>
2023-03-08 23:53:00 -0600

    feat: changed to third

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Archie second time.
+          Edit <code>src/App.js</code> and save to reload Archie third time.
         </p>
         <a
           className="App-link"
